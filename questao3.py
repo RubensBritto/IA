945e148197b018990e397e1507a9300c73d04442
José Rubens <47110177+RubensBritto@users.noreply.github.com>
2022-02-02 16:49:12 -0300

    update

--- questao3.py
+++ questao3.py
@@ -1,12 +1,28 @@
+from types import prepare_class
 import os
-#os.system('clear') 
 
 variaves = []
 valores = []
 se=[]
 entao=[]
 regras=[]
+baseFatos = ['a=s','b=n']
+objetivo = ['c=s']
 
+def buscarObjetivo(lista):
+  for i in lista:
+    if objetivo[0] == i:
+      print(f'Objetivo Localizado')
+      print(f'base de fatos -- {baseFatos}')
+      #POSSO LIMPAR TUDO??
+
+def remove_repetidos(lista):
+    l = []    
+    for i in lista:
+      if i not in l:
+        i = i.replace(" ", "")
+        l.append(i)
+    return l
 
 def buscarVariavel(a):
   if a in variaves:
@@ -31,37 +47,37 @@ def addValoresVariveis():
       valores.append(a)
 
 def selecionarOperadores():
-  a = input(f' Selecione o operador desejado\n 1 - or\n 2 - and\n 3 - entao\n')
-  if a == '1':
-    return 'or'
-  elif a == '2':
-    return 'and'
-  elif a == '3':
-    return 'entao'
-  else:
-    print(f'Operador Invalido, tente novamente')
-    selecionarOperadores()
+  while True:
+    a = input(f' Selecione o operador desejado\n 1 - and\n 2 - entao\n')
+    if a == '1':
+      return 'and'
+    if a == '2':
+      return 'entao'
+    else:
+      print(f'Operador Invalido, tente novamente')
       
 def criarRegras():
   regra = ''
   i = 1
   while True:
-    regraEntao = True if regra[-5:] != 'entao' else False
+    regraEntao = True if regra[-6:-1].replace(" ", "") != 'entao' else False
+    print(f'regraaa - {regra.replace(" ", "")}')
+    print(f'regraaa 5 ultimo - {regra[-6:-1].replace(" ", "")}')
 
     a = input(f"Digite o nome da {i}ª variável: ").lower()
     indice = buscarVariavel(a)
     if indice != -1:
-      regra+=f'{variaves[indice]}={valores[indice]}'
+      regra+=f' {variaves[indice]} = {valores[indice]} '
     else:
       print(f'Variavel não existe')
     
     if regraEntao:
       operador = selecionarOperadores()
       se.append(regra)
-      regra+=f'{operador}'
+      regra+=f'/{operador}/'
       i+=1
     else:
-      e = f'{variaves[indice]}={valores[indice]}'
+      e = f' {variaves[indice]} = {valores[indice]} '
       regras.append(regra)
       entao.append(e)
       i+=1
@@ -69,18 +85,62 @@ def criarRegras():
       print(f'Regra Criada com sucesso: {regra}')
       return menu()
 
+def encadeamentoProgressivo():
+  for r in regras:
+    tempListSe=[]
+    tempListEntao=[]
+    temp = r.split("/entao/")
+    tempListSe.append(temp[0].split("/and/"))
+    tempListSe = remove_repetidos(tempListSe[0])
+    tempListEntao.append(temp[1].split("/and/")[-1])
+    tempListEntao = remove_repetidos(tempListEntao)
+    
+    if set(baseFatos).intersection(tempListSe) and len(set(baseFatos).intersection(tempListSe)) == len(tempListSe):
+      baseFatos.append(tempListEntao[0])
+      buscarObjetivo(baseFatos)
+
+    
+def encadeamentoRegressivo():
+   for i in objetivo:
+     for j in entao:
+       if i == j:
+         baseFatos.append(i)
+         a = entao.index(i)
+         if len(objetivo) == 0:
+           print(f'Encadeamento Regressivo Feito com sucesso, nova base de fatos {baseFatos}')
+         else:
+          objetivo.remove(a)
+          b = se[i].split("/and/")
+          for i in b:
+            i.replace(" ", "")
+            objetivo.append(i)
+   
+   if(len(objetivo)!=0):
+     print(f'Encadeamento Regressivo feito, entranto não encontramos correspodencia para o/os objetivo(s): {objetivo}')      
+
 def menu():
-  print(f'Escolha a Opção desejada\n'
-          '1 - Criar Variaves\n' 
-          '2 - Add Valores Variaves\n'
-          '3 - Criar Regras')
-  a = input("Entre com a opção desejada: ").lower()
-  if a == '1':
-    criarVariveis()
-  elif a == '2':
-    addValoresVariveis()
-  elif a == '3':
-    criarRegras()
+  while True:
+    print(f'Escolha a Opção desejada\n'
+            '1 - Criar Variaves\n' 
+            '2 - Add Valores Variaves\n'
+            '3 - Criar Regras\n'
+            '4 - Encadeamento Para frente\n'
+            '5 - Encadeamento para trás\n'
+            '6 - Encademanento misto\n')
+    a = input("Entre com a opção desejada: ").lower()
+    if a == '1':
+      criarVariveis()
+    elif a == '2':
+      addValoresVariveis()
+    elif a == '3':
+      criarRegras()
+    elif a == '4':
+      encadeamentoProgressivo()
+    elif a == '5':
+      encadeamentoRegressivo()
+    elif a == '6':
+      encadeamentoProgressivo()
+      encadeamentoRegressivo()
 
 def main():
   menu()
